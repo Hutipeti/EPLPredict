71ad6a246205bc887f598108606ef5fc1435ba67
Peter Hutvagner <peter.hutvagner@accenture.com>
2019-06-16 17:41:18 +0200

    extracting the wrongly classified items and expose them is a separate output file

--- epl_predict.py
+++ epl_predict.py
@@ -1,16 +1,19 @@
+import csv
 import pandas as pd
 import data_preprocessing as dp
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 
-#read the csv into dandas dataframe and do some cleaning on it
+#read the csv into pandas dataframe and do some data cleaning on it
 df =  dp.data_preprocessing.process('season-1819_csv.csv')
-#print(df)0
+
+#separate the target feature from the rest
 X = df.drop(columns=['FTR'])
 y = df['FTR'].astype(int)
 
+#split the data for train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 #create the neural network and train it
@@ -19,5 +22,18 @@ mlp.fit(X_train,y_train)
 # run preedictions on the test data and visualize the results
 predictions = mlp.predict(X_test)
 print(confusion_matrix(y_test,predictions))
-print(classification_report(y_test,predictions))
+#print(classification_report(y_test,predictions))
+print(accuracy_score(y_test,predictions))
 
+#iterate over the confusion matrix elements and expose all wrongly predicted datarow in a csv file
+#named wrongly_classified_testdata.csv
+
+# TODO get back the original team names!
+
+misclassified_samples = X_test[y_test != predictions]
+output_mis = misclassified_samples.to_csv(index=False)
+
+f = open("prediction_outcome/wrongly_classified_testdata.csv","w")
+f.write(output_mis)
+f.close()
+print(misclassified_samples)
